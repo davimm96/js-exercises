72a73b8016f362c2c11ba412f65433981606495b
davimm96 <davimichaelsenm@gmail.com>
2021-09-15 17:41:32 -0300

    Exercício message finalizado

---
+++ message/script.js
@@ -0,0 +1,6 @@
+let btnMessage = document.querySelector("#btn-message")
+let message = document.querySelector("#message")
+
+btnMessage.addEventListener('click', function(){
+    message.textContent = "Olá, eu sou programador web Front-end, focado em aprender cada vez mais sobre Javascript puro e suas bibliotecas/frameworks."
+})
