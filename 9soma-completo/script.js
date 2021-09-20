8b59ffc71b7d72e097142756c156b2b4f4e2b2cd
davimm96 <davimichaelsenm@gmail.com>
2021-09-20 16:45:11 -0300

    Acrescentado função no exercício 'Digite dois valores para somar'

--- 9soma-completo/script.js
+++ 9soma-completo/script.js
@@ -3,8 +3,11 @@ let sum2 = document.querySelector("#sum2")
 const submit = document.querySelector("#submit")
 
 submit.addEventListener("click", function(){
+    function conta(a,b){
+        return a + b
+    }
     if(sum1.value.length > 0 && sum1.value.length > 0) {
-        alert(parseInt(sum1.value) + parseInt(sum2.value))
+        alert(conta(parseInt(sum1.value), parseInt(sum2.value)))
     }else {
         alert("digit um número")
     }
