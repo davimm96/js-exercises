1b9fde1eee3cc0c6532b1866888b66e5c3b112c6
davimm96 <davimichaelsenm@gmail.com>
2021-09-19 23:05:26 -0300

    Concertado problemas no exercíco Lampada e CPF

--- 7cpf/script.js
+++ 7cpf/script.js
@@ -3,11 +3,18 @@ let submitCpf = document.querySelector("#submit-cpf")
 let punctuationCpf = document.querySelector("#punctuation-cpf")
 
 submitCpf.addEventListener("click", function(){
-    if(valueCpf.value.length == 11){
-        const cpfFinalScore = valueCpf.value.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, "$1.$2.$3-$4")
+    //const regex = /(\((\d{2})\)\s?)?(\d{4,5})[-]?(\d{4})/gm;
+    //console.log(regex.test('(77) 95684-9783')); //true
+    
+    const cpfFinalScore = valueCpf.value.replace(/(\d{3})?(\d{3})?(\d{3})?(\d{2})/, "$1.$2.$3-$4")
+    //console.log(valueCpf.value.replace(/\D|\-/g, ''))
+    const regex = /\D|\-|\s/gi
+    let test1 = regex.test(valueCpf.value)
+
+    if(valueCpf.value.length == 11 && test1 === false){
         punctuationCpf.innerHTML = cpfFinalScore
     }else {
-        punctuationCpf.innerHTML = "Por favor, digite um CPF válido (11 digitos)."
+        punctuationCpf.innerHTML = "Por favor, digite um CPF válido (11 digitos), sem pontuações."
     }  
 })
 
@@ -15,4 +22,4 @@ submitCpf.addEventListener("click", function(){
 // \d Encontra correspondência com um número. Equivalente a [0-9].
 // ? repetido 0 ou 1 vez
 // {3} qunatidade de digitos
-// $1.$2.$3-$4 ordem
+// $1.$2.$3-$4 ordem e caracteres
