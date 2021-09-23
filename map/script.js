c7817ead55ff0d9890cf7307e82ee891b302423b
davimm96 <davimichaelsenm@gmail.com>
2021-09-23 17:49:23 -0300

    Adicionado novas funcionalidades no exercício 'map'

--- map/script.js
+++ map/script.js
@@ -1,7 +1,12 @@
 const cores = ["vermelho", "azul", "amarelo", "verde"]
+cores.push("rosa", "roxo", "lilás")
 
-const lista = cores.map(function(item,n){
-    n++
-    return alert(n + item)
+
+const lista = cores.map(function(item){
+    return alert(`${item}, possui ${item.length} caracteres`)
 })
+
+// cores.push(novasCores[n])
+
+
 
