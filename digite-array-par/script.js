e8f33454f28dfed8cba9a3349409793625b40e12
davimm96 <davimichaelsenm@gmail.com>
2021-09-24 09:13:52 -0300

    Exercício 'Digite, array, par' Finalizado

---
+++ digite-array-par/script.js
@@ -0,0 +1,22 @@
+let numbs = []
+const subm = document.querySelector("#subm"),
+    numb = document.querySelector("#numb"),
+    ul = document.querySelector("ul")
+    
+subm.addEventListener("click", function(){
+    if(numb.value.length > 0) {
+        ul.innerHTML = "" //reseta a lista para não duplicar
+        numbs.push(numb.value) //atribui o valor digitado no array
+        function lista(item){
+            if(item % 2 == 0){  //par
+                return ul.innerHTML += `<li style="color:red">${item}</li>`
+            }else { //impar
+                return ul.innerHTML += `<li>${item}</li>`
+            }
+        }
+
+        numbs.forEach(lista) //para cada item do array será implementada a função lista
+    }else {
+        alert("Digite um número")
+    }
+})
