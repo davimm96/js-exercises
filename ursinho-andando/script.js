6b921959106c83f03eb24b765e418121399fc9d5
davimm96 <davimichaelsenm@gmail.com>
2021-09-25 10:54:50 -0300

    Exercício 'Ursinho andando' Finalizado

---
+++ ursinho-andando/script.js
@@ -0,0 +1,130 @@
+let n = 0
+const one = document.querySelector("#one")
+let img = document.querySelector("#img")
+ 
+
+setInterval(function(){
+    n++
+    if(n == 1){
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 2) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 3) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 4) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 5) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 6) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 7) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 8) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 9) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 10) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 11) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 12) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 13) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 14) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 15) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 16) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 17) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 18) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 19) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 20) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 21) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 22) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 23) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 24) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 25) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 26) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 27) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 28) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 29) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 30) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 31) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 32) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 33) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 34) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 35) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 36) {
+        img.setAttribute('src', 'img/three.png')
+        img.style.left = n+"0px"
+    }else if(n == 37) {
+        img.setAttribute('src', 'img/one.png')
+        img.style.left = n+"0px"
+    }else if(n == 38) {
+        img.setAttribute('src', 'img/two.png')
+        img.style.left = n+"0px"
+    }else if(n == 39) {
+        img.setAttribute('src', 'img/four.png')
+    }else if(n == 40) {
+        img.setAttribute('src', 'img/four.png')
+    }else if(n == 41) {
+        img.setAttribute('src', 'img/four.png')
+        n = 0
+    }
+},150)
