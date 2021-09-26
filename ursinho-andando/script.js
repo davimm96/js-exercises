13343dc6cf7aa4a0f8ef284b1b507b6f5004e0dd
davimm96 <davimichaelsenm@gmail.com>
2021-09-26 10:48:39 -0300

    Trocado else if por switch case

--- ursinho-andando/script.js
+++ ursinho-andando/script.js
@@ -1,130 +1,50 @@
-let n = 0
 const one = document.querySelector("#one")
-let img = document.querySelector("#img")
+let img = document.querySelector("#img"),
+    n = 0
  
-
 setInterval(function(){
     n++
-    if(n == 1){
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 2) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 3) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 4) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 5) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 6) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 7) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 8) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 9) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 10) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 11) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 12) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 13) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 14) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 15) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 16) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 17) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 18) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 19) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 20) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 21) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 22) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 23) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 24) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 25) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 26) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 27) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 28) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 29) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 30) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 31) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 32) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 33) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 34) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 35) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 36) {
-        img.setAttribute('src', 'img/three.png')
-        img.style.left = n+"0px"
-    }else if(n == 37) {
-        img.setAttribute('src', 'img/one.png')
-        img.style.left = n+"0px"
-    }else if(n == 38) {
-        img.setAttribute('src', 'img/two.png')
-        img.style.left = n+"0px"
-    }else if(n == 39) {
-        img.setAttribute('src', 'img/four.png')
-    }else if(n == 40) {
-        img.setAttribute('src', 'img/four.png')
-    }else if(n == 41) {
-        img.setAttribute('src', 'img/four.png')
-        n = 0
+    switch(n){
+        case 1: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 2: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 3: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 4: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 5: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 6: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 7: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 8: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 9: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 10: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 11: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 12: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 13: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 14: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 15: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 16: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 17: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 18: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 19: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 20: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 21: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 22: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 23: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 24: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 25: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 26: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 27: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 28: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 29: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 30: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 31: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 32: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 33: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 34: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 35: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 36: img.setAttribute('src', 'img/three.png'); img.style.left = n+"0px"; break
+        case 37: img.setAttribute('src', 'img/one.png'); img.style.left = n+"0px"; break
+        case 38: img.setAttribute('src', 'img/two.png'); img.style.left = n+"0px"; break
+        case 39: img.setAttribute('src', 'img/four.png'); break
+        case 40: img.setAttribute('src', 'img/four.png'); break
+        case 41: img.setAttribute('src', 'img/four.png'); n = 0
     }
 },150)
