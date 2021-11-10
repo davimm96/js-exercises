38d540fd517eacdf6c8b1ccea3dc6830a7573b92
davimm96 <davimichaelsenm@gmail.com>
2021-11-10 09:59:00 -0300

    Atualização no exercício 'Menor maior soma' Finalizado

--- menor-maior-soma/script.js
+++ menor-maior-soma/script.js
@@ -1,31 +1,39 @@
 let res = document.querySelector("#res"),
     sub = document.querySelector("#sub"),
-    n1 = document.querySelector("#n1"),
-    n2 = document.querySelector("#n2"),
-    n3 = document.querySelector("#n3")
+    num1 = document.querySelector("#n1"),
+    num2 = document.querySelector("#n2"),
+    num3 = document.querySelector("#n3")
 
 sub.addEventListener("click", function(){
-    n1 = Number(n1.value)
-    n2 = Number(n2.value)
-    n3 = Number(n3.value)
+    let n1 = Number(num1.value),
+        n2 = Number(num2.value),
+        n3 = Number(num3.value)
 
-    res.innerHTML = `Soma: ${n1+n2+n3}<br>`
+    res.innerHTML = ""
 
-    if(n1 > n2 && n1 > n3){
-        res.innerHTML += `${n1} é o maior número<br>`
-    }else if(n2 > n1 && n2 > n3){
-        res.innerHTML += `${n2} é o maior número<br>`
-    }else if(n3 > n1 && n3 > n2){
-        res.innerHTML += `${n2} é o maior número<br>`
+    if(n1 <= 1000 && n1 >= 0 && n2 <= 1000 && n2 >= 0 && n3 <= 1000 && n3 >= 0){
+        res.innerHTML = `Soma: ${n1+n2+n3}<br>`
+
+        if(n1 > n2 && n1 > n3){
+            res.innerHTML += `${n1} é o maior número<br>`
+        }else if(n2 > n1 && n2 > n3){
+            res.innerHTML += `${n2} é o maior número<br>`
+        }else if(n3 > n1 && n3 > n2){
+            res.innerHTML += `${n2} é o maior número<br>`
+        }
+    
+        if(n1 < n2 && n1 < n3){
+            res.innerHTML += `${n1} é o menor número<br>`
+        }else if(n2 < n1 && n2 < n3){
+            res.innerHTML += `${n2} é o menor número<br>`
+        }else if(n3 < n1 && n3 < n2){
+            res.innerHTML += `${n2} é o menor número<br>`
+        }
+    
+    }else {
+        res.innerHTML = "Digite valores entre 1000 e 0"
     }
-
-    if(n1 < n2 && n1 < n3){
-        res.innerHTML += `${n1} é o menor número<br>`
-    }else if(n2 < n1 && n2 < n3){
-        res.innerHTML += `${n2} é o menor número<br>`
-    }else if(n3 < n1 && n3 < n2){
-        res.innerHTML += `${n2} é o menor número<br>`
-    }
-
+    
+    
 
 })
