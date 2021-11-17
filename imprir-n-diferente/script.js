f9e8412bbba4e4c3c7882b619cc46fa8f4a3c65e
davimm96 <davimichaelsenm@gmail.com>
2021-11-16 23:09:48 -0300

    Exercício 'Imprimir n diferente' Finalizado

---
+++ imprir-n-diferente/script.js
@@ -0,0 +1,15 @@
+let res = document.querySelector("#resposta"),
+    sub = document.querySelector("#submit"),
+    numero = document.querySelector("#numero")
+
+sub.addEventListener("click", function(){
+    let num = numero.value
+    res.innerHTML = ""
+
+    for(let x = 1; x <= num; x++){
+        res.innerHTML += "<br>"
+        for(let z = 1; z <= x; z++)
+            res.innerHTML += `${z} `
+    }
+        
+})  
