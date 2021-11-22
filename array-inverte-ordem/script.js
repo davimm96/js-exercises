d1c64265df3a60e30f0fc3dfc4b09c844c1e81c0
davimm96 <davimichaelsenm@gmail.com>
2021-11-22 16:43:01 -0300

    Atualização no exercício '    </head>'

--- array-inverte-ordem/script.js
+++ array-inverte-ordem/script.js
@@ -1,14 +1,9 @@
-let numero1 = document.querySelector("#numero1"),
-    numero2 = document.querySelector("#numero2"),
-    numero3 = document.querySelector("#numero3"),
-    res = document.querySelector("#resposta"),
-    sub = document.querySelector("#submit")
+let numeros = [], res = document.querySelector("#resposta"),
+    qtd = prompt("Quantidade de numeros: ")
 
-sub.addEventListener("click", function(){
-    let num1 = numero1.value,
-        num2 = numero2.value,
-        num3 = numero3.value
-
-    let numeros = [num1, num2, num3]
+for(let x = 1; x <= qtd; x++){
+    numeros.push(prompt("Número: "))
     res.innerHTML = numeros.reverse()
-})
+}
+
+
